43ae7656102b6c8590101e083153508c2f7f792c
arnaud <arnaudmiribel@gmail.com>
2021-08-10 15:28:11 +0200

    Update

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import gpxpy
-
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from gpx_converter import Converter
 import os
 import base64
 
@@ -12,9 +16,25 @@ def get_binary_file_downloader_html(data, file_label="File"):
 
 
 st.title("🍁 Merge `.gpx` files!")
-gpx_files = st.file_uploader(
-    "Upload .gpx files to be merged", type="gpx", accept_multiple_files=True
+st.info(
+    (
+        "This app is useful when you want to merge multiple `.gpx` files.  \n"
+        "In particular, I developed it to be able to merge multiple Strava activities."
+    )
 )
+
+
+CHOICES = {"sample": "Sample .gpx", "upload": "Upload custom .gpx"}
+choice = st.radio("Choose", CHOICES.values(), index=1)
+col1, col2 = st.columns((3, 1))
+
+if choice == CHOICES["upload"]:
+    gpx_files = st.file_uploader(
+        "Upload .gpx files to be merged", type="gpx", accept_multiple_files=True
+    )
+
+elif choice == CHOICES["sample"]:
+    gpx_files = list()
 
 if gpx_files:
     merge = st.button(f"Merge all {len(gpx_files)} uploaded files!")
@@ -41,12 +61,6 @@ if gpx_files:
             unsafe_allow_html=True,
         )
 
-        import plotly.express as px
-        import plotly.graph_objects as go
-        import numpy as np
-        import pandas as pd
-        from gpx_converter import Converter
-
         wonderland = Converter(input_file="merged.gpx").gpx_to_dataframe()
         mapbox_access_token = open(".mapbox_token").read()
         px.set_mapbox_access_token(mapbox_access_token)
@@ -55,11 +69,9 @@ if gpx_files:
             wonderland,
             lat="latitude",
             lon="longitude",
-            # hover_name="time",
             hover_data={
                 "latitude": ":.2f",
                 "longitude": ":.2f",
-                # "alt_ft": ":, ft"
             },
             zoom=11,
             height=500,
@@ -67,7 +79,6 @@ if gpx_files:
 
         fig.update_layout(
             margin=dict(r=0, t=0, l=0, b=0),
-            # mapbox_style="white-bg",
             mapbox=dict(
                 accesstoken=mapbox_access_token,
                 center=go.layout.mapbox.Center(lat=44.8638281, lon=-0.656353),
@@ -75,9 +86,4 @@ if gpx_files:
             ),
         )
 
-        # fig.update_traces(
-        #    hovertemplate="<b>%{hovertext}</b><br><br>(%{customdata[0]:.2f}, %{customdata[1]:.2f})<br>Elev. %{customdata[2]:} .ft<extra></extra>"
-        # )
-
-        # fig.show()
         st.plotly_chart(fig)
